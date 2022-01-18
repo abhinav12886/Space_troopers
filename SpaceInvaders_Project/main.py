4e31c0904ef5156d5270e8554cb29957a2de65e8
Abhinav Sharma <58778689+abhinav12886@users.noreply.github.com>
2022-01-18 12:43:35 +0530

    Update main.py

--- SpaceInvaders_Project/main.py
+++ SpaceInvaders_Project/main.py
@@ -1,11 +1,11 @@
 import pygame # open source module used to create games
-from pygame import mixer
+from pygame import mixer # used for sound effects
 from pygame.locals import *
 import random
 
-# pygame.mixer.pre_init(44100, -16, 2, 512)
-# mixer.init()
-# pygame.init()
+pygame.mixer.pre_init(44100, -16, 2, 512) # pre initialization of mixer (setting up few variables/condition value)
+mixer.init() # initialization of mixer
+pygame.init()
 
 # define fps to limit the fps . Because for some device it will be too fast and for some too slow
 clock = pygame.time.Clock()
@@ -18,16 +18,34 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height)) # creates an untitled window
 pygame.display.set_caption('Space Troopers') # windows top left prints space troopers
 
+# define fonts
+font30 = pygame.font.SysFont('Constantia', 30)
+font40 = pygame.font.SysFont('Constantia', 40)
+
+# load sound
+explosion_fx = pygame.mixer.Sound("img/explosion.wav")
+explosion_fx.set_volume(0.25)
+
+explosion2_fx = pygame.mixer.Sound("img/explosion2.wav")
+explosion2_fx.set_volume(0.25)
+
+laser_fx = pygame.mixer.Sound("img/laser.wav")
+laser_fx.set_volume(0.25)
+
 # define game variables
 rows = 5
 columns = 5
 last_alien_shot = pygame.time.get_ticks()
 alien_cooldown = 1000 # bullet cooldown in milliseconds
+countdown = 3
+last_count = pygame.time.get_ticks()
+game_over = 0 # 0 is no game over, 1 means player has won, -1 means player has lost
 
 
-# define colours(for the health bar)
+# define colours(for the health bar and play and quit button)
 red = (255, 0, 0)
 green = (0, 255, 0)
+white = (255, 255, 255)
 
 # load image
 bg = pygame.image.load("img/bg.png")    # background image loaded
@@ -37,6 +55,11 @@ def draw_bg():
     Function to draw the background
     '''
     screen.blit(bg, (0, 0))  # blit() is used to when we want to put an image on to the screen
+
+# defining function to create image from text(because in pygame we cant directly add text we have to convert it into an image)
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col) # converting text to image
+    screen.blit(img, (x, y)) # image created added to the screen
 
 # create spaceship class
 class Spaceship(pygame.sprite.Sprite):  # child class of Sprite class
@@ -54,6 +77,7 @@ class Spaceship(pygame.sprite.Sprite):  # child class of Sprite class
         speed = 8
         # set a cooldown variable
         cooldown = 100 # milliseconds
+        game_over = 0
 
         # get key press
         key = pygame.key.get_pressed()
@@ -67,6 +91,7 @@ class Spaceship(pygame.sprite.Sprite):  # child class of Sprite class
 
         # shoot
         if key[pygame.K_SPACE] and time_now - self.last_shot > cooldown:  # if space key is pressed
+            laser_fx.play()
             bullet = Bullets(self.rect.centerx, self.rect.top) # creates a new bullet and put it at the coordinates
             Bullets_group.add(bullet) # and add it to the group.
             self.last_shot = time_now
@@ -83,6 +108,8 @@ class Spaceship(pygame.sprite.Sprite):  # child class of Sprite class
             explosion = Explosion(self.rect.centerx, self.rect.centery, 2)
             explosion_group.add(explosion)
             self.kill()
+            game_over = -1
+        return game_over
 
 
 
@@ -100,6 +127,7 @@ class Bullets(pygame.sprite.Sprite):
             self.kill()
         if pygame.sprite.spritecollide(self, aliens_group, True): # it is an inbuilt function to detect collision
             self.kill() # kills the bullet also when there is a collison btw alien and bullet
+            explosion_fx.play()
             explosion = Explosion(self.rect.centerx, self.rect.centery, 2)
             explosion_group.add(explosion)
 
@@ -133,10 +161,11 @@ class Alien_Bullets(pygame.sprite.Sprite):
         self.rect.center = [x, y]
 
     def update(self):
-        self.rect.y += 2  # it will keep moving the bullets after creation of it by pressing spacebar
-        if self.rect.top > screen_height:  # kills the bullets after bullet bottom  reaching 50 pixels below the top
+        self.rect.y += 2
+        if self.rect.top > screen_height:
             self.kill()
         if pygame.sprite.spritecollide(self, spaceship_group, False, pygame.sprite.collide_mask): # False = we dnt want the spaceship to get destroyed completely in just one bullet
+            explosion2_fx.play()
             self.kill()
             # reduce spaceship health
             spaceship.health_remaining -= 1 # using spaceship object we are reducing health
@@ -199,8 +228,6 @@ def create_alien():
 
 create_alien()
 
-
-
 # create player
 spaceship = Spaceship(int(screen_width / 2), screen_height - 100, 3) # last arguent '3' define how many times the player can be hit.
 spaceship_group.add(spaceship)
@@ -214,29 +241,45 @@ while run: # till its true the game will continue to run
     #draw background
     draw_bg() # it will add background to the screen but it doesnt update the screen automatically
 
-    # create random alien bullets
-    # record current time
-    time_now = pygame.time.get_ticks()
+    if countdown == 0:
+        # create random alien bullets
+        # record current time
+        time_now = pygame.time.get_ticks()
 
-    # shoot
-    if time_now - last_alien_shot > alien_cooldown and len(aliens_bullets_group) < 5 and len(aliens_group) > 0: #
-        attacking_alien = random.choice(aliens_group.sprites())
-        alien_bullet = Alien_Bullets(attacking_alien.rect.centerx, attacking_alien.rect.bottom)
-        aliens_bullets_group.add(alien_bullet)
-        last_alien_shot = time_now
+        # shoot
+        if time_now - last_alien_shot > alien_cooldown and len(aliens_bullets_group) < 5 and len(aliens_group) > 0:
+            attacking_alien = random.choice(aliens_group.sprites())
+            alien_bullet = Alien_Bullets(attacking_alien.rect.centerx, attacking_alien.rect.bottom)
+            aliens_bullets_group.add(alien_bullet)
+            last_alien_shot = time_now
 
-    # event handlers
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:   # cross 'x' button on top right when clicked game terminates
-            run = False     # condition becomes false and gets out of loop and game stops
+        # check if all the aliens has been killed
+        if len(aliens_group) == 0:
+            game_over = 1
 
-    # update spaceship
-    spaceship.update()
+        if game_over == 0:  # if the game is not over
+            # update spaceship
+            game_over = spaceship.update()  # if spaceship is destroyed agme_over == -1
 
-    # update sprite groups
-    Bullets_group.update()
-    aliens_group.update()
-    aliens_bullets_group.update()
+            # update sprite groups
+            Bullets_group.update()
+            aliens_group.update()
+            aliens_bullets_group.update()
+        else:
+            if game_over == -1:
+                draw_text("GAME OVER", font40, white, int(screen_width / 2 - 100), int(screen_height / 2 + 50))
+            if game_over == 1:
+                draw_text("YOU WIN", font40, white, int(screen_width / 2 - 100), int(screen_height / 2 + 50))
+
+    if countdown > 0:
+        draw_text("GET READY", font40, white, int(screen_width/2 - 110), int(screen_height/2 + 50))
+        draw_text(str(countdown), font40, white, int(screen_width/2 - 10), int(screen_height/2 + 100))
+        count_timer = pygame.time.get_ticks()
+        if count_timer - last_count > 1000:
+            countdown -= 1
+            last_count = count_timer
+
+    # update explosion group
     explosion_group.update()
 
     # draw sprite group
@@ -246,9 +289,16 @@ while run: # till its true the game will continue to run
     aliens_bullets_group.draw(screen)
     explosion_group.draw(screen)
 
+    # event handlers
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:   # cross 'x' button on top right when clicked game terminates
+            run = False     # condition becomes false and gets out of loop and game stops
+
     pygame.display.update()  # for automatic background updation we use it
 
 pygame.quit()
+
+
 
 
 
